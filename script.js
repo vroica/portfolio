604fafd0cb845aa06dd60f49702ec60c3f40110c
vroica <156434028+vroica@users.noreply.github.com>
2024-03-17 08:13:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 const colors = ['#f0f6ff', '#fffcf2', '#b6eefc', '#c0baff', '#ffbdca'];
-let randomColor = colors[Math.floor(Math.random()*(colors.length))];
-console.log(randomColor);
-let button = document.getElementsByTagName('button');
+let button = document.getElementById('button');
 function changeColor() { 
+    let randomColor = colors[Math.floor(Math.random()*(colors.length))];
+    console.log(randomColor);
     document.querySelector('body').style.backgroundColor = randomColor;
 }
 button.onclick = changeColor;
